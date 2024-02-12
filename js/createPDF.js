6897b55694d416b7cb53c328b7cad8a321600409
anastasia <anastasia.gribovskaia@gmail.com>
2024-02-12 23:00:31 +0300

    fix: change a request function to server

--- js/createPDF.js
+++ js/createPDF.js
@@ -1,11 +1,34 @@
 const API_URL = `http://localhost:5000/createPDF`;
 
+// export default function createPDF() {
+//     axios.post(API_URL)
+//         .then((res) => {
+//             console.log('res:', res)
+//         })
+//         .catch((e) => {
+//             console.log(e)
+//         })
+// }
+
 export default function createPDF() {
-    axios.post(API_URL)
-        .then((res) => {
-            console.log(res)
-        })
-        .catch((e) => {
-            console.log(e)
-        })
+    fetch(API_URL, {
+        // data: {
+        //     invoiceDetails,
+        //     invoiceSettings,
+        //     itemsDetails,
+        //     organisationInfos,
+        //     otherDetails,
+        //     clientDetails
+        // },
+        method: 'POST'
+    }).then(async res => {
+        console.log('res:', res)
+        return res
+            .arrayBuffer()
+            .then(res => {
+                const blob = new Blob([res], { type: 'application/pdf' })
+                saveAs(blob, 'invoice.pdf')
+            })
+            .catch(e => alert(e))
+    })
 }
