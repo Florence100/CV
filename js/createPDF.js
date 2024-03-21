5635fb75162ce4b05bdad5d712ef7a08cfa9be19
anastasia <Anastasiya.Gribovskaya@forge.by>
2024-03-21 12:09:18 +0300

    feat: add loading animation

--- js/createPDF.js
+++ js/createPDF.js
@@ -1,6 +1,22 @@
 const API_URL = `https://cv-server-dtfy.onrender.com/createPDF`;
 
 export default function createPDF() {
+    const loader = document.querySelector('.loader');
+    const downloadPdf = document.querySelector('.download-pdf');
+
+    const animation = lottie.loadAnimation({
+        container: loader,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './assets/animation/loader_2.json',
+    });
+
+    downloadPdf.style.display = 'none';
+    loader.style.display= 'block';
+    animation.setSpeed(0.8);
+    animation.play();
+
     fetch(API_URL, {
         method: 'GET',
     }).then(async res => {
@@ -15,5 +31,9 @@ export default function createPDF() {
                 })
     }).catch((e) => {
         alert(e)
+    }).finally(() => {
+        animation.destroy();
+        loader.style.display= 'none';
+        downloadPdf.style.display = 'block';
     })
 }
